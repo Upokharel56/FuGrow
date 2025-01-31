b66bff016993b39ecc96725fa4a964904796f920
Upokharel56 <155223739+Upokharel56@users.noreply.github.com>
2025-01-31 19:34:07 +0545

    Db utils

---
+++ dbUtils/curd.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once "dbcon.php";
+
+function getData($table, $columns, $condition = "1")
+{
+    global $conn;
+    $cols = implode(", ", $columns);
+    $sql = "SELECT $cols FROM $table WHERE $condition";
+
+    $result = $conn->query($sql);
+    $data = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+
+    return $data;
+}
+
+function putData($table, $data)
+{
+    global $conn;
+    $columns = implode(", ", array_keys($data));
+    $values = implode(", ", array_map(function ($val) {
+        return "'" . addslashes($val) . "'";
+    }, array_values($data)));
+
+    $sql = "INSERT INTO $table ($columns) VALUES ($values)";
+
+    return $conn->query($sql);
+}
+
+
+function updateData($table, $data, $condition = "1") {
+    global $conn;
+    $setValues = [];
+    foreach ($data as $column => $value) {
+        $setValues[] = "$column = '" . addslashes($value) . "'";
+    }
+
+    $setString = implode(", ", $setValues);
+    $sql = "UPDATE $table SET $setString WHERE $condition";
+
+    return $conn->query($sql);
+}
+
+function deleteData($table, $condition = "1") {
+    global $conn;
+    $sql = "DELETE FROM $table WHERE $condition";
+
+    return $conn->query($sql);
+}
+
